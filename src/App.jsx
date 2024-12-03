0de0d36e177f2d6deb509a8a9cdf4c40af16d545
AlexZ955 <alex.zhao1024@gmail.com>
2024-12-03 16:06:57 +0800

    feat: add components features, chip and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import './App.css'
 import { Navbar } from './components/Navbar'
 import { Hero } from './components/Hero'
 import { Highlights } from './components/Highlights'
 import { Model } from './components/Model'
+import { Features } from './components/Features'
+import { Chip } from './components/Chip'
+import { Footer } from './components/Footer'
 
 const App = () => {
     return (
@@ -11,6 +13,9 @@ const App = () => {
             <Hero />
             <Highlights />
             <Model />
+            <Features />
+            <Chip />
+            <Footer />
         </main>
     )
 }
